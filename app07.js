b2b69848099244f2045533370907f3254c1446ed
tb0ied <pchonacki@proton.me>
2023-09-14 10:55:41 +0100

    0607

---
+++ app07.js
@@ -0,0 +1,16 @@
+console.log("One day or Another");
+
+let username = prompt("What is your name");
+console.log(username); 
+function greet(){
+let time = prompt("What time is it?");
+return document.write("Hello " + username );
+}
+ //< less than
+ //we want to say good morning
+ if (time < 11){
+    console.log("good morning");
+} else if  (time <= 17)
+    console.log("Good afternoon");
+ }
+
---
+++ demo.js
@@ -0,0 +1,28 @@
+console.log ("Hello world");
+
+//this is a comment, anything i write here won't effect the code
+//variables
+
+let myName = "Tim";
+let myAge = 900;
+
+ //data types
+ let string = "Anything withing quotes";
+ let number = 10;
+ let boolean = true; //or false
+
+ //prompt
+ //here we ask  the user a question
+ let cheese = prompt("Hey,how are you");
+ // here we tell the user their answer
+ console.log(cheese);
+
+ //if
+ let time = prompt("What is the time?(hh in 24 hour format please");
+ //< less than
+ //we want to say good morning
+ if (time < 12){
+    console.log("good morning");
+} else {
+    console.log("Good afternoon");
+ }
